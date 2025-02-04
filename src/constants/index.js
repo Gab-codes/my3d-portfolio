d8af9e63b82764953a1e8346843e1967b9716f4b
Temidayo Gabriel <dayotoru@gmail.com>
2025-02-04 06:01:30 -0500

    changes made

--- src/constants/index.js
+++ src/constants/index.js
@@ -71,7 +71,7 @@ export const myProjects = [
       boxShadow: "0px 4px 20px 0px rgba(30, 58, 138, 0.5)",
     },
 
-    spotlight: "/assets/andrew-portfolio-spotlight.png",
+    spotlight: "/assets/idea-bulb.png",
     tags: [
       {
         id: 1,
